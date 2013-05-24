9d93ebc12c1855ce6c67a24725339e9eee947746
salzkraftwerk <co-github.com@einseinself.org>
2013-05-24 17:56:13 +0200

    unzip update.

--- common/unzip_all_in_folder.sh
+++ common/unzip_all_in_folder.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # unzip all zip content includet in current dir
 # zip including files will be unpacked into subfolder named like the zip file
-for zipfiles in $(find . -name *.zip)
+#for zipfiles in $(find -name *.zip .)
+for zipfiles in *.zip
 do
   mkdir $(basename $zipfiles .zip)
-  unzip -d $(basename $zipfiles .zip) $zipfiles
+  unzip  -d $(basename $zipfiles .zip) $zipfiles
 done
